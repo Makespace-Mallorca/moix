5a7dafe7ddd4b4370655dc29c01b7bd1027588ee
Carlos Orts <mcorts69@gmail.com>
2024-06-09 08:45:06 +0200

    v0

--- moix.ino
+++ moix.ino
@@ -1,59 +1,257 @@
-//#include "CubeCell_NeoPixel.h"
-#include "Adafruit_NeoPixel.h"
+/**
+ *
+ * Carlos Orts
+ */
+#include "LoRaWanMinimal_APP.h"
+#include <CayenneLPP.h>//the library is needed https://github.com/ElectronicCats/CayenneLPP
+#include "Arduino.h"
+#include "settings.h"
 
+#define SWITCH_PIN GPIO4 // signal to switch
 
-#define PIN     GPIO1   // Pin driving DIN of first NeoPixel
-#define NUMPIXELS 7   // Number of NeoPixels in pixels or ring
-#define DELAYVAL 100  // Time (in milliseconds) to pause between pixels
-#define BRIGHTNESS 50 // Brightness of NeoPixel
+int loops; // number of readings
+float cycles; // number of read and transmisions cycles.
+float icycles; // delta for cycles counter.
+float noise_avg; // noise level average
+float noise_avg_pre; // noise level average previous
+unsigned int noise_peak; // noise peak value
+unsigned int noise_min; // noise minimun value
+unsigned int noise; // current noise value
+unsigned long noise_sum; // noise level addition
 
-//CubeCell_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ400);
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+uint16_t battery;
 
-void setup() {
-  Serial.begin(115200);
-  // put your setup code here, to run once:
-  pinMode(Vext,OUTPUT);
-  digitalWrite(Vext,LOW); //SET POWER
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  pixels.clear(); // Set all pixel colors to 'off'
+unsigned long tmp_ini; 
+long CountStart = 0;
+long now_DutyCycle = 0;
 
-  pixels.begin(); // This initializes the NeoPixel library. 
-  delay(500);
-  pixels.fill(pixels.Color(0, 0, 0),0, pixels.numPixels()); // black  
-  Serial.println("LightOFF");
+CayenneLPP lpp(LORAWAN_APP_DATA_MAX_SIZE);//if use AT mode, don't modify this value or may run dead https://github.com/HelTecAutomation/CubeCell-Arduino/search?q=commissioning.h
 
-  pixels.show(); // This sends the updated pixel color to the hardware.
-  delay(500);
-  pixels.fill(pixels.Color(255, 255, 255),0, pixels.numPixels()); // black  
-  Serial.println("LightOn");
+///////////////////////////////////////////////////
+//Some utilities for going into low power mode
+TimerEvent_t sleepTimer;
+//Records whether our sleep/low power timer expired
+bool sleepTimerExpired;
+
+static void wakeUp()
+{
+  sleepTimerExpired=true;
 }
 
-uint8_t i=0, j=0;
+static void lowPowerSleep(uint32_t sleeptime)
+{
+  sleepTimerExpired=false;
+  TimerInit( &sleepTimer, &wakeUp );
+  TimerSetValue( &sleepTimer, sleeptime );
+  TimerStart( &sleepTimer );
+  //Low power handler also gets interrupted by other timers
+  //So wait until our timer had expired
+  while (!sleepTimerExpired) lowPowerHandler();
+  TimerStop( &sleepTimer );
+}
 
+///////////////////////////////////////////////////
+void setup() {
+	Serial.begin(115200);
+
+  // initialize digital pin for LED as an output.
+  pinMode(SWITCH_PIN, OUTPUT);
+  digitalWrite(SWITCH_PIN, LOW);
+
+  // This enables (with LOW) the output to power the sensor
+  pinMode(Vext, OUTPUT);
+  digitalWrite(Vext, LOW);
+  Serial.printf(" | Vext: ");
+  Serial.println(digitalRead(Vext));
+  delay(500);
+
+  // Sensor identification
+  Serial.printf(" | SensorId: ");
+  Serial.println(SensorId);
+
+  tmp_ini = millis(); 
+  noise_avg = 0;
+  noise_avg_pre = LowNoiseLevel;
+  noise_peak = 0;
+  noise_min = 1000; 
+  noise_sum = 0;
+  loops = 0;
+  cycles = 50;
+  icycles = 1;
+  noise_avg = LowNoiseLevel;
+
+  if (ACTIVE_REGION==LORAMAC_REGION_AU915) {
+    //TTN uses sub-band 2 in AU915
+    LoRaWAN.setSubBand2();
+  }
+ 
+  LoRaWAN.begin(LORAWAN_CLASS, ACTIVE_REGION);
+  
+  //Enable ADR
+  LoRaWAN.setAdaptiveDR(true);
+
+  while (1) {
+    Serial.print("Joining... ");
+    LoRaWAN.joinOTAA(appEui, appKey, devEui);
+    if (!LoRaWAN.isJoined()) {
+      //In this example we just loop until we're joined, but you could
+      //also go and start doing other things and try again later
+      Serial.println("JOIN FAILED! Sleeping for 60 seconds");
+      lowPowerSleep(60000);
+    } else {
+        Serial.println("JOINED");
+        if (LORAWAN_CLASS == CLASS_A) {
+          Serial.println("CLASS A Confirmed");
+        } else {
+           Serial.println("ERROR MOT CLASS A");
+        }
+      break;
+    }
+  }
+
+}
+
+void transmitRecord()
+{
+  /*
+  * set LoraWan_RGB to Active,the RGB active in loraWan
+  * RGB red means sending;
+  * RGB purple means joined done;
+  * RGB blue means RxWindow1;
+  * RGB yellow means RxWindow2;
+  * RGB green means received done;
+  */
+ 
+  // Cycles control
+  if (cycles>99) {
+    icycles = -1 ;
+  } else if (cycles<1){
+    icycles = +1 ;
+  }
+  cycles += icycles;
+   
+  // Cayenne
+  lpp.reset();
+  lpp.addAnalogInput(1,SensorId);
+  lpp.addAnalogInput(2,cycles);
+  lpp.addAnalogInput(3,battery);
+  lpp.addLuminosity(1, noise_avg);
+  lpp.addLuminosity(2, noise_peak);
+  lpp.addLuminosity(3, noise_min);    
+  lpp.addGPS(2, latitude, longitude, alt);
+
+  Serial.println("Transmiting...");
+  Serial.print("lpp data size: ");
+  Serial.print(lpp.getSize());
+  Serial.println();
+  
+  // Request ACK only for the 3 firts transmision of a cycle
+  bool requestack=cycles<3?true:false;
+  
+  // if (LoRaWAN.send(1, lpp.getBuffer(), lpp.getSize(), requestack)) {
+  if (LoRaWAN.send(lpp.getSize(), lpp.getBuffer(), 2, requestack)) {
+    Serial.println("Send OK");
+  } else {
+    Serial.println("Send FAILED");
+  }
+}
+///////////////////////////////////////////////////
 void loop() {
-    pixels.setPixelColor(j, pixels.Color(i, 0, 0));
+  // Noise reading each second
+  if (digitalRead(Vext) == 0) {
+    noise = analogReadmV(ADC);
+  } else {
+      digitalWrite(Vext, LOW);
+  }
 
-    pixels.show();   // Send the updated pixel colors to the hardware.
+  if (noise > 4500) {
+    Serial.print("outlier removed: ");
+    Serial.println(noise);
+ } else {
+    if (millis() - tmp_ini > 1000) {
+      noise_sum += noise;
+      loops ++;
+      Serial.print("Noise: ");
+      Serial.print(noise);
+      Serial.print(" loop: ");
+      Serial.print(loops);
+      Serial.print(" cycles: ");
+      Serial.println(cycles);
+      tmp_ini = millis(); 
+      // TEST COLOR LEDS
+      if (noise > 2000 ) {
+          digitalWrite(SWITCH_PIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+          Serial.println(digitalRead(SWITCH_PIN));
+          delay(5000);                      // wait for a second
+          digitalWrite(SWITCH_PIN, LOW);   // turn the LED off by making the voltage LOW
+          Serial.println(digitalRead(SWITCH_PIN));
+          delay(5000); 
+      }
+    }
+    if (noise > noise_peak) {
+      noise_peak = noise;
+      Serial.print("Noise peak: ");
+      Serial.println(noise_peak);
+    }
+    if (noise < noise_min) {
+      noise_min = noise;
+      Serial.print("Noise min: ");
+      Serial.println(noise_min);
+    }
+  }
 
-    delay(200); // Pause before next pass through loop
+  long now_DutyCycle = millis();
+  if (now_DutyCycle - CountStart > DutyCycle) {
+    CountStart = now_DutyCycle;
 
-    pixels.setPixelColor(j, pixels.Color(0, i, 0));
+    // Noise calculations
+    noise_avg = int(noise_sum / loops);
+    Serial.print("Noise average: ");
+    Serial.println(noise_avg);
+    Serial.println(noise_sum);
+    Serial.println(loops);
 
-    pixels.show();   // Send the updated pixel colors to the hardware.
+    // Swith off-on the power of the sensor in order to read the battery
+    digitalWrite(Vext, HIGH);
+    delay(1000);
+    battery = getBatteryVoltage();
+    digitalWrite(Vext, LOW);
+    Serial.print("Battery: ");
+    Serial.println(battery);
+    delay(100);
 
-    delay(200); // Pause before next pass through loop
+    transmitRecord();
 
-    pixels.setPixelColor(j, pixels.Color(0, 0, i));
+    noise_peak = 0;
+    noise_min = 1000;
+    noise_sum = 0;
+    loops = 0;
+  
+    // Low Noise Mode if two Noise Overage are under LowNoiseLevel
+    if (noise_avg < LowNoiseLevel && noise_avg_pre < LowNoiseLevel) {
+      if (LORAWAN_CLASS == CLASS_A) {
+        cycles -= icycles;
+        digitalWrite(Vext, HIGH);
+        Serial.println("Switch OFF Micro");
+        Serial.println("Sleep");
+        delay(400);
 
-    pixels.show();   // Send the updated pixel colors to the hardware.
+        lowPowerSleep(Sleep4NoNoise); 
+        digitalWrite(Vext, LOW);
+        Serial.println("Switch ON Micro");
+      }
+    } 
+    noise_avg_pre = noise_avg;
+  }
+}
 
-    delay(200); // Pause before next pass through loop
-    pixels.setPixelColor(j, pixels.Color(0, 0, 0));
-    
-    i+=10;
-    j++;
-    if(j>15)
-      j=0;
-
+///////////////////////////////////////////////////
+//Example of handling downlink data
+void downLinkDataHandle(McpsIndication_t *mcpsIndication)
+{
+  Serial.printf("Received downlink: %s, RXSIZE %d, PORT %d, DATA: ",mcpsIndication->RxSlot?"RXWIN2":"RXWIN1",mcpsIndication->BufferSize,mcpsIndication->Port);
+  for(uint8_t i=0;i<mcpsIndication->BufferSize;i++) {
+    Serial.printf("%02X",mcpsIndication->Buffer[i]);
+  }
+  Serial.println();
 }
---
+++ settings.h
@@ -0,0 +1,20 @@
+// LoRaWAN Configuration
+
+/* OTAA para*/
+static uint8_t appEui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static uint8_t devEui[] = { 0x6A, 0x01, 0x1E, 0xFE, 0x1D, 0x85, 0xF2, 0xDF };
+static uint8_t appKey[] = { 0xD6, 0x57, 0x6C, 0x9B, 0xE3, 0xD6, 0xDA, 0x05, 0xDD, 0x7A, 0x4C, 0xA3, 0x60, 0xA2, 0x70, 0xB5 };
+
+uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
+static DeviceClass_t lorawanClass = LORAWAN_CLASS;
+
+// Devise location
+const float latitude = 39.634708582824864;
+const float longitude = 2.6343323359171507;
+const int alt = -5;
+
+// Other params
+float SensorId= 12.05;// Sensor  identifcator number 
+const int DutyCycle = 600000; // Transmision and reading period
+const int Sleep4NoNoise = 300000; // Sleep period when low noise 
+const int LowNoiseLevel = 37; // Limit for low noise mode
